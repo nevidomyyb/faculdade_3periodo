67bd7d692a1d942cab0d0c3a289ec4797a256eeb
Pedro Cunha <idk.pedroc@gmail.com>
2023-09-02 00:25:12 -0300

    corrigido roteamento media e implementado modelçs

--- projeto_integrador/main_site/models.py
+++ projeto_integrador/main_site/models.py
@@ -2,5 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 
+def send(instance, filename):
+    return f"images/{filename}"
 class Projeto(models.Model):
-    ...
+    idprojeto = models.AutoField(primary_key=True)
+    titulo = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=255)
+    imagem = models.ImageField(upload_to=send)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+    
--- projeto_integrador/main_site/views.py
+++ projeto_integrador/main_site/views.py
@@ -1,23 +1,17 @@
 from django.shortcuts import render
 from django.contrib.staticfiles import finders
 from django.shortcuts import render
+from main_site.models import Projeto
 # Create your views here.
 
 def home(request, *args, **kwargs):
     home_css_path = finders.find('main_site/css/home.css')
     template_path = 'main_site/home.html'
 
+
     context = {
         "projetos": [
-            {"titulo": "TituloA", "descricao": "DescriacoA"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
-            {"titulo": "TituloB", "descricao": "DescriacoB"},
+            {"titulo": projeto.titulo, "imagem": "media/"+projeto.imagem.path, "descricao": projeto.descricao[0:len(projeto.descricao)-10]} for projeto in Projeto.objects.all()
         ]
     }
 
