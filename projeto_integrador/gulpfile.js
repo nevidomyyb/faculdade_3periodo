7c5605cb10215f338d3156e6d5b2a27ce6602f5a
Pedro Cunha <idk.pedroc@gmail.com>
2023-09-01 21:12:12 -0300

    corrigindo paths

--- projeto_integrador/gulpfile.js
+++ projeto_integrador/gulpfile.js
@@ -2,11 +2,11 @@
     const sass = require('gulp-sass')(require('sass'));
 
     function compilaSass(){
-        return gulp.src('./src/styles/*.scss')
+        return gulp.src('./editor_main/src/styles/*.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./dist/styles'))
+        .pipe(gulp.dest('./main_site/static/main_site/css/'))
     }
     exports.default = gulp
     exports.watch = function(){
-        gulp.watch('./src/styles/*.scss', gulp.series(compilaSass));
+        gulp.watch('./editor_main/src/styles/*.scss', gulp.series(compilaSass));
     }
